b636ef8d51a86309519ea11bce8589b3f76f5997
jozzi05 <jozzi05@pc>
2023-08-20 00:10:23 +0200

    chore: add regression tests before refactoring

--- spec/trust_in_spec.rb
+++ spec/trust_in_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-require 'spec_helper'
+
 require 'trust_in'
 
 RSpec.describe TrustIn do
-  describe '#update_score', :vcr do
+  describe '#update_score' do
     subject!(:update_score) { described_class.new(evaluations).update_score }
 
     context "when the evaluation type is 'SIREN'" do
@@ -14,6 +14,14 @@ RSpec.describe TrustIn do
 
         it 'decreases the <score> of 5' do
           expect(evaluations.first.score).to eq(74)
+        end
+      end
+
+      context 'with a <score> greater or equal to 50 AND the <state> is favorable' do
+        let(:evaluations) { [Evaluation.new(type:, value: '123456789', score: 79, state: 'favorable', reason: 'unable_to_reach_api')] }
+
+        it 'decreases the <score> by 1' do
+          expect(evaluations.first.score).to eq(78)
         end
       end
 
@@ -34,22 +42,58 @@ RSpec.describe TrustIn do
       end
 
       context "when the <state> is 'unconfirmed' AND the <reason> is 'ongoing_database_update'" do
-        let(:evaluations) { [Evaluation.new(type:, value: '832940670', score: 42, state: 'unconfirmed', reason: 'ongoing_database_update')] }
+        let(:evaluations) { [Evaluation.new(type:, value: evaluation_value, score: 42, state: 'unconfirmed', reason: 'ongoing_database_update')] }
 
-        it 'assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100', :aggregate_failures do
-          expect(evaluations.first.state).to eq('favorable')
-          expect(evaluations.first.reason).to eq('company_opened')
-          expect(evaluations.first.score).to eq(100)
+        context "when API returns <company state> as 'Actif'", vcr: { cassette_name: 'services/sirene-v3/actif' } do
+          let(:evaluation_value) { '832940670' }
+
+          it 'assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100' do
+            expect(evaluations.first).to have_attributes(
+              state: 'favorable',
+              reason: 'company_opened',
+              score: 100
+            )
+          end
+        end
+
+        context "when API returns <company state> as not 'Actif'", vcr: { cassette_name: 'services/sirene-v3/not_actif' } do
+          let(:evaluation_value) { '320878499' }
+
+          it 'assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100' do
+            expect(evaluations.first).to have_attributes(
+              state: 'unfavorable',
+              reason: 'company_closed',
+              score: 100
+            )
+          end
         end
       end
 
       context 'with a <score> equal to 0' do
-        let(:evaluations) { [Evaluation.new(type: type, value: '320878499', score: 0, state: 'favorable', reason: 'company_opened')] }
+        let(:evaluations) { [Evaluation.new(type:, value: evaluation_value, score: 0, state: 'favorable', reason: 'company_opened')] }
 
-        it 'assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100', :aggregate_failures do
-          expect(evaluations.first.state).to eq('unfavorable')
-          expect(evaluations.first.reason).to eq('company_closed')
-          expect(evaluations.first.score).to eq(100)
+        context "when API returns <company state> as 'Actif'", vcr: { cassette_name: 'services/sirene-v3/actif' } do
+          let(:evaluation_value) { '832940670' }
+
+          it 'assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100' do
+            expect(evaluations.first).to have_attributes(
+              state: 'favorable',
+              reason: 'company_opened',
+              score: 100
+            )
+          end
+        end
+
+        context "when API returns <company state> as not 'Actif'", vcr: { cassette_name: 'services/sirene-v3/not_actif' } do
+          let(:evaluation_value) { '320878499' }
+
+          it 'assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100' do
+            expect(evaluations.first).to have_attributes(
+              state: 'unfavorable',
+              reason: 'company_closed',
+              score: 100
+            )
+          end
         end
       end
 
