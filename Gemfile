84dbf63e60f4f1ff6a2fdd3c7330d801b7673d15
jozzi05 <jozzi05@pc>
2023-08-19 21:30:23 +0200

    Add rubocop and fix simple issues

--- Gemfile
+++ Gemfile
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
-gem "rspec", "~>3.9"
+gem 'rspec', '~>3.9'
 gem "json", "~>2.2"
+
+group :development, :test do
+  gem 'rubocop'
+  gem 'rubocop-rspec'
+end
--- lib/trust_in.rb
+++ lib/trust_in.rb
@@ -1,57 +1,55 @@
-require "json"
-require "net/http"
+# frozen_string_literal: true
+
+require 'json'
+require 'net/http'
 
 class TrustIn
   def initialize(evaluations)
     @evaluations = evaluations
   end
 
-  def update_score()
+  def update_score
     @evaluations.each do |evaluation|
-      if evaluation.type == "SIREN"
-        if evaluation.score > 0 && evaluation.state == "unconfirmed" && evaluation.reason == "ongoing_database_update"
-          uri = URI("https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene_v3" \
+      if evaluation.type == 'SIREN'
+        if evaluation.score.positive? && evaluation.state == 'unconfirmed' && evaluation.reason == 'ongoing_database_update'
+          uri = URI('https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene_v3' \
             "&q=#{evaluation.value}&sort=datederniertraitementetablissement" \
-            "&refine.etablissementsiege=oui")
+            '&refine.etablissementsiege=oui')
           response = Net::HTTP.get(uri)
           parsed_response = JSON.parse(response)
-          company_state = parsed_response["records"].first["fields"]["etatadministratifetablissement"]
-          if company_state == "Actif"
-            evaluation.state = "favorable"
-            evaluation.reason = "company_opened"
-            evaluation.score = 100
+          company_state = parsed_response['records'].first['fields']['etatadministratifetablissement']
+          evaluation.score = 100
+
+          if company_state == 'Actif'
+            evaluation.state = 'favorable'
+            evaluation.reason = 'company_opened'
           else
-            evaluation.state = "unfavorable"
-            evaluation.reason = "company_closed"
-            evaluation.score = 100
+            evaluation.state = 'unfavorable'
+            evaluation.reason = 'company_closed'
           end
         elsif evaluation.score >= 50
-          if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api"
+          if evaluation.state == 'unconfirmed' && evaluation.reason == 'unable_to_reach_api'
             evaluation.score = evaluation.score - 5
-          elsif evaluation.state == "favorable"
+          elsif evaluation.state == 'favorable'
             evaluation.score = evaluation.score - 1
           end
-        elsif evaluation.score <= 50 && evaluation.score > 0
-          if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api" || evaluation.state == "favorable"
-            evaluation.score = evaluation.score - 1
-          end
-        else
-          if evaluation.state == "favorable" || evaluation.state == "unconfirmed"
-            uri = URI("https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene_v3" \
+        elsif evaluation.score <= 50 && evaluation.score.positive?
+          evaluation.score = evaluation.score - 1 if evaluation.state == 'unconfirmed' && evaluation.reason == 'unable_to_reach_api' || evaluation.state == 'favorable'
+        elsif evaluation.state == 'favorable' || evaluation.state == 'unconfirmed'
+          uri = URI('https://public.opendatasoft.com/api/records/1.0/search/?dataset=sirene_v3' \
               "&q=#{evaluation.value}&sort=datederniertraitementetablissement" \
-              "&refine.etablissementsiege=oui")
-            response = Net::HTTP.get(uri)
-            parsed_response = JSON.parse(response)
-            company_state = parsed_response["records"].first["fields"]["etatadministratifetablissement"]
-            if company_state == "Actif"
-              evaluation.state = "favorable"
-              evaluation.reason = "company_opened"
-              evaluation.score = 100
-            else
-              evaluation.state = "unfavorable"
-              evaluation.reason = "company_closed"
-              evaluation.score = 100
-            end
+              '&refine.etablissementsiege=oui')
+          response = Net::HTTP.get(uri)
+          parsed_response = JSON.parse(response)
+          company_state = parsed_response['records'].first['fields']['etatadministratifetablissement']
+          evaluation.score = 100
+
+          if company_state == 'Actif'
+            evaluation.state = 'favorable'
+            evaluation.reason = 'company_opened'
+          else
+            evaluation.state = 'unfavorable'
+            evaluation.reason = 'company_closed'
           end
         end
       end
@@ -70,7 +68,7 @@ class Evaluation
     @reason = reason
   end
 
-  def to_s()
+  def to_s
     "#{@type}, #{@value}, #{@score}, #{@state}, #{@reason}"
   end
 end
