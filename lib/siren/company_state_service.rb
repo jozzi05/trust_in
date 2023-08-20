fa45db2b3b6e7a4f3d44709f196b037b423433d6
jozzi05 <jozzi05@pc>
2023-08-20 13:55:55 +0200

    chore: Wrap CompanyStateService with Siren module

--- lib/siren/company_state_service.rb
+++ lib/siren/company_state_service.rb
@@ -3,18 +3,20 @@
 require 'json'
 require 'net/http'
 
-class CompanyStateService
-  ACTIVE_STATE = 'Actif'
+module Siren
+  class CompanyStateService
+    ACTIVE_STATE = 'Actif'
 
-  def self.active?(value)
-    uri = URI(
-      'https://public.opendatasoft.com/api/records/1.0/search/?dataset=economicref-france-sirene-v3' \
-      "&q=#{value}&sort=datederniertraitementetablissement" \
-      '&refine.etablissementsiege=oui'
-    )
-    response = Net::HTTP.get(uri)
-    parsed_response = JSON.parse(response)
-    state = parsed_response['records'].first['fields']['etatadministratifetablissement']
-    state == ACTIVE_STATE
+    def self.active?(value)
+      uri = URI(
+        'https://public.opendatasoft.com/api/records/1.0/search/?dataset=economicref-france-sirene-v3' \
+          "&q=#{value}&sort=datederniertraitementetablissement" \
+          '&refine.etablissementsiege=oui'
+      )
+      response = Net::HTTP.get(uri)
+      parsed_response = JSON.parse(response)
+      state = parsed_response['records'].first['fields']['etatadministratifetablissement']
+      state == ACTIVE_STATE
+    end
   end
 end
